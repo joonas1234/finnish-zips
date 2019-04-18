2c871ab75e67b4f103b1ee3de3cf9eecd000fb4f
Spacha <miikasikala96@gmail.com>
2019-04-18 03:52:49 +0300

    Added config file

--- config/finnish-zips.php
+++ config/finnish-zips.php
@@ -1,5 +1,17 @@
 <?php
 
 return [
-    //
+
+	/*
+    |--------------------------------------------------------------------------
+    | Silent Mode
+    |--------------------------------------------------------------------------
+    |
+    | When this option is set to true, no exceptions are thrown, if given zip
+    | code is null or otherwise invalid. If invalid zip code is provided,
+    | getArea() method returns an empty string and getAreaKey returns null.
+    |
+    */
+
+    'silent' => true
 ];
